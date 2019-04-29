f7402fae9612e833c26806f17011ac6694910973
Nisha Shaikh <nishashareefshaikh@gmail.com>
2019-04-29 12:49:13 +0500

    minor changes

--- Code/GeneticAlgorithm.py
+++ Code/GeneticAlgorithm.py
@@ -4,12 +4,13 @@ from chromosome import chromosome
 from constants import *
 import random
 
-#Take user inputs to allow controlling parameters 
-ITER=int(input("Enter the number of iterations: "))
-POP_SIZE=int(input("Enter population size: "))
-MUTATION_RATE=float(input("Enter mutation rate between 0 and 1: "))
-GENERATIONS=int(input("Enter number of generations: "))
-OFFSPRINGS=int(input("Enter number of offsprings: "))
+# Take user inputs to allow controlling parameters
+ITER = int(input("Enter the number of iterations: "))
+POP_SIZE = int(input("Enter population size: "))
+MUTATION_RATE = float(input("Enter mutation rate between 0 and 1: "))
+GENERATIONS = int(input("Enter number of generations: "))
+OFFSPRINGS = int(input("Enter number of offsprings: "))
+
 
 def Initialise_Population(pop_size):
     '''Initializes chromosones to construct the initial population'''
@@ -17,6 +18,8 @@ def Initialise_Population(pop_size):
     for i in range(pop_size):
         pop.append(chromosome())
     return pop
+
+
 '''
 SELECTION SCHEME
 As a learning outcome from Assignment 01,
@@ -25,44 +28,40 @@ Parent Selection : Truncation
 Survivor Selection: Truncation
 '''
 
-def Truncation(population,Sel):#Sel signifies P or S selection where P is parent and S is survivor
-    if (Sel=="P"):#parent selection
-        Parents=[]
 
+# Sel signifies P or S selection where P is parent and S is survivor
+def Truncation(population, Sel):
+    if (Sel == "P"):  # parent selection
+        Parents = []
 
         return Parents
-    elif (Sel=="S"):
-        Top_fit=[]
+    elif (Sel == "S"):
+        Top_fit = []
 
         return Top_fit
     else:
         print("Selection is not identified")
 
 
-def Crossover(parent1,parent2):
+def Crossover(parent1, parent2):
     pass
 
 
-def Mutate(population,rate):
+def Mutate(population, rate):
     for i in range(0, len(population)):
-        myRandom=round(random.uniform(0,1), 2)#rounded off to 2 dp
-        if (myRandom<rate):
+        myRandom = round(random.uniform(0, 1), 2)  # rounded off to 2 dp
+        if (myRandom < rate):
             pass
 
     return population
+
 
 def Evolve():
     pass
 
 
 def main():
-    myMelodies=Initialise_Population(POP_SIZE)
-    
-
-
-
-
-
+    myMelodies = Initialise_Population(POP_SIZE)
 
 
 main()
